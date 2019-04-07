0ed6d3f3c7110546271d6ea4c26d789944dd7bf0
William Primett <wprim001@gold.ac.uk>
2019-04-07 22:35:36 +0100

    data buffer script

--- data_handler.js
+++ data_handler.js
@@ -97,11 +97,6 @@ function get_ws_dict(dev_data)
 	}
 }
 
-function bang()
-{
-
-}
-
 function list(){
 	if (inlet == 0) {
 		var device_data = arrayfromargs(arguments);
@@ -172,45 +167,3 @@ function charCodeString2string (s){
 		}
 		return s2;
 	}
-
-//taken from example: https://codepen.io/bave8782/pen/dXKvrW	
-class CircularBuffer {
-  constructor(size) {
-    this.memory = new Array(size);
-    this.head = 0;
-    this.tail = 0;
-    this.isFull = false;
-  }
-  
-  read() {
-    if (this.tail === this.head && !this.isFull) {
-      console.log('Nothing to read.');
-    } else {
-      this.tail = this.next(this.tail);
-      this.isFull = false;
-      return this.memory[this.tail];
-    }
-  }
-  
-  write(value) {
-    if (this.isFull) {
-      console.error('Buffer full');
-      return;
-    } else {
-      this.head = this.next(this.head);
-      this.memory[this.head] = value;
-      if (this.head === this.tail) {
-        this.isFull = true;
-      }
-    }
-  }
-  
-  next(n) {
-    var nxt = n + 1;
-    if (nxt === this.memory.length) {
-      return 0;
-    } else {
-      return nxt;
-    }
-  }
-}
